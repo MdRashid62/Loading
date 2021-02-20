47fd5ec9fc7f306dc653034f2f14bc5adea38c3a
MdRashid62 <rashy.mr62@gmail.com>
2021-02-20 23:09:11 +0500

    final

--- script.js
+++ script.js
@@ -4,5 +4,10 @@ function incNum() {
   const line = document.getElementById("line");
 
   let a = window.getComputedStyle(line, ":before").getPropertyValue("width");
-  a
+  a = Math.floor((parseInt(a) / 10) * 2);
+  text.innerHTML = a + "%";
+  console.log(a);
+  if (a == 100) {
+    clearInterval(op);
+  }
 }
